ee573d13d17a8a8ca833887d1a7c496bdacc5eb4
Ivailo Iliev <ivailoiliev89@gmail.com>
2026-05-04 11:25:57 +0300

    Create TestCase for fishing_place and make ngingx conf -- from static to staticfiles to collectstatic.

--- fishing_app/tests.py
+++ fishing_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import FishingPlace
 
-# Create your tests here.
+
+class FishingPlaceTest(TestCase):
+    def setUp(self):
+        FishingPlace.objects.create(
+            name="Gradina Lake", description="Many kind of fishes here")
+
+    def test_place_content(self):
+        place = FishingPlace.objects.get(id=1)
+        self.assertEqual(place.name, 'Gradina Lake')
+        self.assertEqual(place.description, "Many kind fishes here")
+
+    def test_homepage_status(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
